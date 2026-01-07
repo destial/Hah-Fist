c8bc11cc6ae5dc45e4b5e2c58b8bd739cc12ef0e
Rance Andres <67675805+destial@users.noreply.github.com>
2026-01-07 13:09:06 +0800

    Added more AEVec2 operator overloading

--- Source/Utils/AEOverload.h
+++ Source/Utils/AEOverload.h
@@ -2,6 +2,7 @@
 #ifndef __AEOVERLOAD_H
 #define __AEOVERLOAD_H
 #include "AEMath.h"
+#include <iostream>
 
 static AEVec2 operator+ (const AEVec2& lhs, const AEVec2& rhs) {
 	return { lhs.x + rhs.x, lhs.y + rhs.y };
@@ -9,6 +10,14 @@ static AEVec2 operator+ (const AEVec2& lhs, const AEVec2& rhs) {
 
 static AEVec2 operator- (const AEVec2& lhs, const AEVec2& rhs) {
 	return { lhs.x - rhs.x, lhs.y - rhs.y };
+}
+
+static AEVec2 operator* (const AEVec2& lhs, f32 magnitude) {
+	return { lhs.x * magnitude, lhs.y * magnitude };
+}
+
+static AEVec2 operator/ (const AEVec2& lhs, f32 magnitude) {
+	return { lhs.x / magnitude, lhs.y / magnitude };
 }
 
 static AEVec2& operator+= (AEVec2& lhs, const AEVec2& rhs) {
@@ -21,6 +30,23 @@ static AEVec2& operator-= (AEVec2& lhs, const AEVec2& rhs) {
 	lhs.x -= rhs.x;
 	lhs.y -= rhs.y;
 	return lhs;
+}
+
+static AEVec2& operator*= (AEVec2& lhs, f32 magnitude) {
+	lhs.x *= magnitude;
+	lhs.y *= magnitude;
+	return lhs;
+}
+
+static AEVec2& operator/= (AEVec2& lhs, f32 magnitude) {
+	lhs.x /= magnitude;
+	lhs.y /= magnitude;
+	return lhs;
+}
+
+static std::ostream& operator<< (std::ostream& os, const AEVec2& rhs) {
+	os << '{' << rhs.x << ", " << rhs.y << '}';
+	return os;
 }
 
 #endif
