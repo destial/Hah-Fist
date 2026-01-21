979eb21020e09d1236e57ff2ffd75a3f82d6f78e
Rance Andres <67675805+destial@users.noreply.github.com>
2026-01-21 10:49:32 +0800

    Update GameObjectEntity.cpp

--- Source/Entities/GameObjectEntity.cpp
+++ Source/Entities/GameObjectEntity.cpp
@@ -1,4 +1,5 @@
 #include "GameObjectEntity.hpp"
+#include "../Utils/AEOverload.hpp"
 
 GameObjectEntity::GameObjectEntity() : health(1.f),damage(1.f), isActive(true), type(CIRCLE), BaseEntity({0.f})
 {
@@ -23,8 +24,16 @@ void GameObjectEntity::Update(const f32& dt)
 	BaseEntity::Update(dt);
 }
 
-void GameObjectEntity::PostUpdate(const f32& dt)
+void GameObjectEntity::PostUpdate(const f32& dt) 
 {
+	this->position += this->velocity * dt;
+	this->position.y = AEClamp(this->position.y, this->scale.y * 0.5f, Utils::GetWorldHeight() - (this->scale.y * 0.5f));
+	/*if (this->position.x <= 0.f) {
+		velocity.x = 0;
+	}*/
+	if (this->position.y <= this->scale.y * 0.5f) {
+		velocity.y = 0;
+	}
 	BaseEntity::PostUpdate(dt);
 }
 
