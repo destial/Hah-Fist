dbcf9eddd2d140b10813005b85d259ec61545e72
destial <destialpvp@gmail.com>
2026-03-10 15:02:39 +0800

    Added more documentation & comments

--- Source/Entities/StaticEntity.hpp
+++ Source/Entities/StaticEntity.hpp
@@ -9,7 +9,11 @@ public:
 		TYPE_PLATFORM
 	}static_type;
 public:
-	StaticEntity(STATIC_TYPE static_type,AEVec2 pos = { 0.f,0.f }, f32 mass = 1.f, CollisionShape shape = CollisionShape::AABB, PhysicsType go_type = PhysicsType::STATIC);
+	StaticEntity(STATIC_TYPE static_type,AEVec2 pos = { 0.f,0.f }, f32 mass = 1.f, CollisionShape shape = CollisionShape::AABB, PhysicsType go_type = PhysicsType::STATIC); // Ctor
+	StaticEntity(StaticEntity const&) = delete; // Remove copy ctor
+	StaticEntity& operator=(StaticEntity const&) = delete; // Remove copy assignment
+	virtual ~StaticEntity(); // Dtor
+
 	void PreUpdate(const f32& dt) override;
 	void Update(const f32& dt) override;
 	void PostUpdate(const f32& dt) override;
