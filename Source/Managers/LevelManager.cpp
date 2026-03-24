4c156f0e71793211f74d14b8fe6348d709316a04
destial <destialpvp@gmail.com>
2026-03-24 22:26:53 +0800

    Added tutorial text
    - Added volume control in main menu

--- Source/Managers/LevelManager.cpp
+++ Source/Managers/LevelManager.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "LevelManager.hpp"
+#include "../UI/ImageUI.hpp"
 #include <fstream>
 #include <cstdio>
 
@@ -64,6 +65,117 @@ namespace LevelManager {
 		}
 		in_tutorial = true;
 		// TODO: load tutorial
+
+		if (level == 0) {
+			ImageUI* hold = new ImageUI{ ASSET_HUD_IMAGE, {-80.f, 16.f} };
+			hold->layer = BaseEntity::RenderLayer::WORLD;
+			hold->scale = { 15.f, 2.5f };
+			hold->text_size = 8.f;
+			hold->text = "Hold left mouse button to charge your fist!";
+			scene->AddEntityToScene(hold);
+
+			ImageUI* release = new ImageUI{ ASSET_HUD_IMAGE, {-80.f, 13.f} };
+			release->layer = BaseEntity::RenderLayer::WORLD;
+			release->scale = { 15.f, 2.5f };
+			release->text_size = 8.f;
+			release->text = "Release the mouse to use your attack!";
+			scene->AddEntityToScene(release);
+
+			ImageUI* turbo = new ImageUI{ ASSET_HUD_IMAGE, {-80.f, 10.f} };
+			turbo->layer = BaseEntity::RenderLayer::WORLD;
+			turbo->scale = { 15.f, 2.5f };
+			turbo->text_size = 8.f;
+			turbo->text = "This is your turbo fist!";
+			scene->AddEntityToScene(turbo);
+
+			ImageUI* jump = new ImageUI{ ASSET_HUD_IMAGE, {5.f, 21.f} };
+			jump->layer = BaseEntity::RenderLayer::WORLD;
+			jump->scale = { 15.f, 2.5f };
+			jump->text_size = 8.f;
+			jump->text = "Charge your first to get past!";
+			scene->AddEntityToScene(jump);
+
+			ImageUI* crate = new ImageUI{ ASSET_HUD_IMAGE, {47.f, 8.f} };
+			crate->layer = BaseEntity::RenderLayer::WORLD;
+			crate->scale = { 15.f, 2.5f };
+			crate->text_size = 8.f;
+			crate->text = "This is a breakable crate with loot!";
+			scene->AddEntityToScene(crate);
+
+			ImageUI* slime = new ImageUI{ ASSET_HUD_IMAGE, {106.f, 20.f} };
+			slime->layer = BaseEntity::RenderLayer::WORLD;
+			slime->scale = { 15.f, 2.5f };
+			slime->text_size = 8.f;
+			slime->text = "Kill the slimes to get more coins!";
+			scene->AddEntityToScene(slime);
+
+			ImageUI* boss = new ImageUI{ ASSET_HUD_IMAGE, {168.f, 23.f} };
+			boss->layer = BaseEntity::RenderLayer::WORLD;
+			boss->scale = { 15.f, 2.f };
+			boss->text_size = 8.f;
+			boss->text = "Next is the boss room!";
+			scene->AddEntityToScene(boss);
+
+			ImageUI* next = new ImageUI{ ASSET_HUD_IMAGE, {168.f, 20.f} };
+			next->layer = BaseEntity::RenderLayer::WORLD;
+			next->scale = { 15.f, 2.f };
+			next->text_size = 8.f;
+			next->text = "Kill the boss to advance!";
+			scene->AddEntityToScene(next);
+		}
+
+		if (level == 1) {
+			ImageUI* cycle = new ImageUI{ ASSET_HUD_IMAGE, {16.f, 16.f} };
+			cycle->layer = BaseEntity::RenderLayer::WORLD;
+			cycle->scale = { 15.f, 2.5f };
+			cycle->text_size = 8.f;
+			cycle->text = "Cycle between '1', '2' to switch your fists!";
+			scene->AddEntityToScene(cycle);
+
+			ImageUI* grapple = new ImageUI{ ASSET_HUD_IMAGE, {16.f, 13.f} };
+			grapple->layer = BaseEntity::RenderLayer::WORLD;
+			grapple->scale = { 15.f, 2.5f };
+			grapple->text_size = 8.f;
+			grapple->text = "The grapple fist pulls enemies to you!";
+			scene->AddEntityToScene(grapple);
+
+			ImageUI* combo = new ImageUI{ ASSET_HUD_IMAGE, {16.f, 10.f} };
+			combo->layer = BaseEntity::RenderLayer::WORLD;
+			combo->scale = { 15.f, 2.5f };
+			combo->text_size = 8.f;
+			combo->text = "Try combo-ing your grapple and turbo fist!";
+			scene->AddEntityToScene(combo);
+
+			ImageUI* fall = new ImageUI{ ASSET_HUD_IMAGE, {112.f, 10.f} };
+			fall->layer = BaseEntity::RenderLayer::WORLD;
+			fall->scale = { 15.f, 2.5f };
+			fall->text_size = 8.f;
+			fall->text = "Don't fall in the void!";
+			scene->AddEntityToScene(fall);
+
+			ImageUI* spider = new ImageUI{ ASSET_HUD_IMAGE, {194.f, 13.f} };
+			spider->layer = BaseEntity::RenderLayer::WORLD;
+			spider->scale = { 15.f, 2.5f };
+			spider->text_size = 8.f;
+			spider->text = "Spiders spawn babies when killed!";
+			scene->AddEntityToScene(spider);
+		}
+
+		if (level == 2) {
+			ImageUI* finger = new ImageUI{ ASSET_HUD_IMAGE, {8.f, 7.f} };
+			finger->layer = BaseEntity::RenderLayer::WORLD;
+			finger->scale = { 15.f, 2.5f };
+			finger->text_size = 8.f;
+			finger->text = "Hotkey '3' is your finger gun!";
+			scene->AddEntityToScene(finger);
+
+			ImageUI* archer = new ImageUI{ ASSET_HUD_IMAGE, {51.f, 18.f} };
+			archer->layer = BaseEntity::RenderLayer::WORLD;
+			archer->scale = { 15.f, 2.5f };
+			archer->text_size = 8.f;
+			archer->text = "This is an archer! Ranged enemy!";
+			scene->AddEntityToScene(archer);
+		}
 	}
 
 	bool IsInTutorial() {
--- Source/Scenes/StartMenuScene.cpp
+++ Source/Scenes/StartMenuScene.cpp
@@ -8,6 +8,7 @@
 #include "../Utils/Utils.hpp"
 #include "../Utils/Constant.hpp"
 #include "../UI/ButtonUI.hpp"
+#include "../UI/BarUI.hpp"
 #include "../UI/ImageUI.hpp"
 #include "../UI/CircleButtonUI.hpp"
 #include "../Managers/SceneManager.hpp"
@@ -60,7 +61,7 @@ void StartMenuScene::Init() {
 	});
 	fist->scale = { BACKGROUND_FIST_SCALE, BACKGROUND_FIST_SCALE };
 
-	ImageUI* fist2 = new ImageUI{ ASSET_GRAPPLEFIST_IMAGE, { 10.f, Utils::GetWorldHeight() * 0.25f } };
+	ImageUI* fist2 = new ImageUI{ ASSET_GRAPPLEFIST_IMAGE, { BACKGROUND_FIST_SCALE * 2.f, Utils::GetWorldHeight() * 0.25f } };
 	fist2->AddUpdateListener(this, [fist2](const f32& dt) {
 		if (fist2->scale.x > 0) {
 			fist2->position.x += dt * BACKGROUND_FIST_SPEED;
@@ -77,7 +78,7 @@ void StartMenuScene::Init() {
 	});
 	fist2->scale = { BACKGROUND_FIST_SCALE, BACKGROUND_FIST_SCALE };
 
-	ImageUI* fist3 = new ImageUI{ ASSET_FINGERGUN_IMAGE, { -10.f, Utils::GetWorldHeight() * 0.75f } };
+	ImageUI* fist3 = new ImageUI{ ASSET_FINGERGUN_IMAGE, { -BACKGROUND_FIST_SCALE * 2.f, Utils::GetWorldHeight() * 0.75f } };
 	fist3->AddUpdateListener(this, [fist3](const f32& dt) {
 		if (fist3->scale.x > 0) {
 			fist3->position.x += dt * BACKGROUND_FIST_SPEED;
@@ -234,6 +235,40 @@ void StartMenuScene::Init() {
 	});
 	scene_entities.push_back(unlock_all);
 #endif
+
+	BarUI* master_vol = new BarUI({ Utils::GetWorldWidth() - 7.5f, 5.f });
+	master_vol->text = "";
+	master_vol->SetValue(1.f);
+	master_vol->overlay_color = {255, 64, 128, 64};
+	master_vol->scale = { BUTTON_SCALE_X * 0.75f, BUTTON_SCALE_Y * 0.35f };
+	master_vol->text_size = 7.5f;
+	master_vol->text_alignment = BaseUI::TEXT_ALIGNMENT::LEFT_CORNER;
+	master_vol->AddUpdateListener(this, [master_vol](const f32 &dt) {
+		AEAudioSetGroupVolume(Game::GetMusicGroup(), AEClamp(master_vol->GetValue(), 0.f, 1.f));
+		char vol[64];
+		sprintf_s(vol, 64, "Master Volume: %0.2f", master_vol->GetValue() * 100.f);
+		master_vol->text = vol;
+
+		master_vol->position.x = Utils::LerpCircle(Utils::GetWorldWidth() - 7.5f, -7.5f, level_panel);
+	});
+	scene_entities.push_back(master_vol);
+
+	BarUI* sfx_vol = new BarUI({ Utils::GetWorldWidth() - 7.5f, 3.f });
+	sfx_vol->text = "";
+	sfx_vol->SetValue(1.f);
+	sfx_vol->overlay_color = { 255, 64, 128, 64 };
+	sfx_vol->scale = { BUTTON_SCALE_X * 0.75f, BUTTON_SCALE_Y * 0.35f };
+	sfx_vol->text_size = 7.5f;
+	sfx_vol->text_alignment = BaseUI::TEXT_ALIGNMENT::LEFT_CORNER;
+	sfx_vol->AddUpdateListener(this, [sfx_vol](const f32& dt) {
+		AEAudioSetGroupVolume(Game::GetSfxGroup(), AEClamp(sfx_vol->GetValue(), 0.f, 1.f));
+		char vol[64];
+		sprintf_s(vol, 64, "SFX Volume: %0.2f", sfx_vol->GetValue() * 100.f);
+		sfx_vol->text = vol;
+
+		sfx_vol->position.x = Utils::LerpCircle(Utils::GetWorldWidth() - 7.5f, -7.5f, level_panel);
+	});
+	scene_entities.push_back(sfx_vol);
 
 	scene_entities.push_back(start);
 	scene_entities.push_back(quit);
